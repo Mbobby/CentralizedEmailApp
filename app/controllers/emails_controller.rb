7082e0c53109cb9c1ec06f1b305377a0b1b11d03
Emmanuel Mong <mongemmanuel@gmail.com>
2015-08-27 02:04:34 -0400

    After adding email send functionality

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -10,14 +10,41 @@ class EmailsController < ApplicationController
 	end
 
 	def show
+		@email = Email.find(:id)
 	end
-	# def create
-	# end
+	
+	def create
+		@email = Email.new(email_params)
+		@user = User.find_by_username(params['username'])
+		if(@user)
+			@email.users << @user
+		else
+			flash[:notice] = "User with username : #{params['username']} does not exist"
+			redirect_to '/send'
+			return
+		end
+
+		if(@email.save)
+			flash[:notice] = "Email Sent successfully to #{params['username']}"
+			redirect_to '/inbox'
+			return
+		else
+			flash[:notice] = "Email could not be sent to #{params['username']}, Please try again"
+			redirect_to '/send'
+			return
+		end	
+	end
 
 	# def destroy
 	# end
 
+
+	def sent
+		@emails = Email.where(authorId: current_user.id)
+	end
+	
+	private
 	def email_params
-		params.require(:email).permit(:authorId, :title,:body, :username)
+		params.require(:email).permit(:authorId, :title,:body)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'inbox' => 'emails#index'
   get 'inbox/:id' => 'emails#show'
   get 'send' => 'emails#new'
+  get 'sent' => 'emails#sent'
   post 'send' => 'emails#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
